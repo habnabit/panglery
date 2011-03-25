8ecb4eefa0b9790fb77a609f67a44f37da0db027
Aaron Gallagher <habnabit@gmail.com>
2011-03-24 21:31:10 -0700

    Adding docs.

--- panglery/pangler.py
+++ panglery/pangler.py
@@ -12,6 +12,22 @@ class Pangler(object):
     Panglers store hooks for events and dispatch to these hooks when an event
     is triggered. Event parameters can be modified or added.
 
+    Panglers also implement the descriptor protocol. If a Pangler is fetched as
+    an instance attribute (iff it was originally a class attribute), a new
+    Pangler will be produced which is bound to the relevant instance. Like
+    bound methods, bound Panglers will pass `self` as the first parameter to
+    their event hooks when triggered.
+
+    Bound Panglers are also cached; fetching a Pangler from the same instance
+    twice will produce the same bound Pangler both times. This is useful when
+    trying to add new hooks per-instance.
+
+    Normally, an instance can only have one Pangler bound to it in this way
+    because of the way bound Panglers are cached. To allow an instance to have
+    multiple Panglers bound and cached, one must pass the `id` parameter when
+    instantiating a Pangler. The provided `id` will be used as the cache key
+    instead of the default.
+
     """
 
     _bound_pangler_cache = weakref.WeakKeyDictionary()
@@ -71,23 +87,55 @@ class Pangler(object):
                 hook.execute(self, event)
 
     def clone(self):
+        """Duplicate a Pangler.
+
+        Returns a copy of this Pangler, with all the same state. Both will be
+        bound to the same instance and have the same `id`, but new hooks will
+        not be shared.
+
+        """
+
         p = type(self)(self.id)
         p.hooks = list(self.hooks)
         p.instance = self.instance
         return p
 
     def combine(self, *others):
+        """Combine other Panglers into this Pangler.
+
+        Returns a copy of this Pangler with all of the hooks from the provided
+        Panglers added to it as well. The new Pangler will be bound to the same
+        instance and have the same `id`, but new hooks will not be shared with
+        this Pangler or any provided Panglers.
+
+        """
+
         p = self.clone()
         for other in others:
             p.hooks.extend(other.hooks)
         return p
 
     def bind(self, instance):
+        """Bind an instance to this Pangler.
+
+        Returns a clone of this Pangler, with the only difference being that
+        the new Pangler is bound to the provided instance. Both will have the
+        same `id`, but new hooks will not be shared.
+
+        """
+
         p = self.clone()
         p.instance = instance
         return p
 
     def cached_bind(self, instance):
+        """Bind an instance to this Pangler, using the bound Pangler cache.
+
+        This method functions identically to `bind`, except that it might
+        return a Pangler which was previously bound to the provided instance.
+
+        """
+
         cache = self._bound_pangler_cache.get(instance)
         if cache is None:
             # If we don't use a weak value dictionary here, the strong
@@ -107,11 +155,27 @@ class Pangler(object):
 
     @classmethod
     def from_cache(cls, instance, id=_DEFAULT_ID):
+        """Fetch a bound Pangler from the cache.
+
+        Returns a Pangler which was previously bound to the provided instance
+        with the provided cache key. If no Pangler was previously bound, this
+        method raises a KeyError.
+
+        """
+
         return cls._bound_pangler_cache[instance][id]
 
 _DEFAULT_AGGREGATE_ID = object()
 
 class PanglerAggregate(object):
+    """A means for aggregating Panglers.
+
+    PanglerAggregates will walk the MRO of a class, collecting Panglers which
+    are exposed as a class attribute with a particular name, and combine them
+    together into one Pangler.
+
+    """
+
     def __init__(self, attr_name=None, id=_DEFAULT_AGGREGATE_ID):
         super(PanglerAggregate, self).__init__()
         self.attr_name = attr_name
